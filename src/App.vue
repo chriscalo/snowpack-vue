86e8895b80577ba2f7827a779d9508cf84d8ea2f
Chris Calo <ccalo@google.com>
2020-06-21 11:24:36 -0400

    create Counter.vue component

--- src/App.vue
+++ src/App.vue
@@ -5,30 +5,17 @@
       Edit <code>src/App.vue</code> and save to reload.
     </p>
   </header>
-  <p>
-    <button @click="counter.decrement">−</button>
-    <span class="counter">{{ format.number(counter.value) }}</span>
-    <button @click="counter.increment">+</button>
-    <button @click="counter.reset">Reset</button>
-    <button @click="counter.value += 100">Jump 100</button>
-  </p>
+  <Counter/>
 </template>
  
 <script>
-  import format from "./format.js";
-  import useCounter from "./use/counter.js";
+  import Counter from "./components/Counter.vue";
+  
+  console.log(Counter)
   
   export default {
-    setup() {
-      const counter = useCounter(0);
-      console.log(counter);
-      
-      setInterval(counter.increment, 1000);
-      
-      return {
-        counter,
-        format,
-      };
+    components: {
+      Counter,
     },
   };
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="counter.decrement">−</button>
+  <span class="counter">{{ format.number(counter.value) }}</span>
+  <button @click="counter.increment">+</button>
+  <button @click="counter.reset">Reset</button>
+  <button @click="counter.value += 100">Jump 100</button>
+</template>
+
+<script>
+  import format from "../format.js";
+  import useCounter from "../use/counter.js";
+  
+  export default {
+    setup() {
+      const counter = useCounter(0);
+      console.log(counter);
+      
+      setInterval(counter.increment, 1000);
+      
+      return {
+        counter,
+        format,
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
